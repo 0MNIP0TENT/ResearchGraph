66567b68e309ce3d9c2d510eb5e56c0e2f126bb8
0MNIP0TENT <kkiminsky@gmail.com>
2022-06-27 13:05:47 -0400

    change charts ui

--- Charts/urls.py
+++ Charts/urls.py
@@ -3,7 +3,6 @@ from . import views
 
 app_name = 'Charts'
 urlpatterns = [
-    path('', views.charts_view, name='charts'),
     path('degree/', views.display_degree_view, name='degree'),
     path('relation/', views.display_relation_view, name='relation'),
 ]

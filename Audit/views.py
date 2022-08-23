4a92c0dd4d340ed2dc53610f5d40a3074a2c475b
0MNIP0TENT <kkiminsky@gmail.com>
2022-08-23 18:02:35 -0400

    compare speed

--- Audit/views.py
+++ Audit/views.py
@@ -158,7 +158,8 @@ class TripleList(ListView):
     def get_queryset(self):
         # original qs
         qs = super().get_queryset() 
-        return qs.filter(user=self.request.user)
+        #return qs.filter(user=self.request.user)
+        return self.request.user.triple_set 
 
 class TripleCreate(CreateView):
     model = Triple
@@ -188,8 +189,7 @@ class TripleUpdate(UpdateView):
     def get_queryset(self):
         # original qs
         qs = super().get_queryset() 
-        #return qs.filter(user=self.request.user)
-        return self.request.user.triple_set 
+        return qs.filter(user=self.request.user)
 
     def get_success_url(self):
         return reverse('Audit:triple_list')

d32bda725f15f6136abb04841ab3719f86182be2
0MNIP0TENT <kkiminsky@gmail.com>
2022-11-06 15:38:07 -0500

    exclude types in django admin

--- Audit/admin.py
+++ Audit/admin.py
@@ -5,6 +5,7 @@ from .models import Type, AuditTriple, Dataset
 
 @admin.register(AuditTriple)
 class AuditAdmin(admin.ModelAdmin): 
+    exclude = ('entityA_types','entityB_types',)
     list_display = ('dataset','user','entityA','relation','entityB','verified',)
     list_filter = ('user','relation','entityA','entityB','verified',)
 

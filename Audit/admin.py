c95d957a75989c212c79aaba786f8dd135cd431f
0MNIP0TENT <kkiminsky@gmail.com>
2022-11-06 15:29:34 -0500

    add filter in django admin

--- Audit/admin.py
+++ Audit/admin.py
@@ -2,6 +2,11 @@ from django.contrib import admin
 from .models import Type, AuditTriple, Dataset
 
 # Register your models here.
+
+@admin.register(AuditTriple)
+class AuditAdmin(admin.ModelAdmin): 
+    list_display = ('dataset','user','entityA','relation','entityB','verified',)
+    list_filter = ('user','relation','entityA','entityB','verified',)
+
 admin.site.register(Type)
-admin.site.register(AuditTriple)
 admin.site.register(Dataset)
